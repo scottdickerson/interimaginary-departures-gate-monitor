099325c951e2f4acc1ffc0796041dc5b3d57d955
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2020-02-16 10:07:35 -0600

    fix(flights): backend update

--- src/FlightsAPI.js
+++ src/FlightsAPI.js
@@ -2,9 +2,9 @@ import { normalizeFlight } from "./dataUtils";
 import moment from "moment";
 
 export const fetchFlights = minutesToSeparate =>
-  fetch("http://localhost:8080/flights", {
+  fetch("http://192.168.7.120:8080/flights", {
     method: "get",
-    url: `http://localhost:8080`
+    url: `http://192.168.7.120:8080`
   }).then(response => {
     console.log("fetch response");
     return response.json().then(flights =>
