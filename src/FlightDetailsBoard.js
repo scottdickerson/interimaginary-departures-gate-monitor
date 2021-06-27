7b867f615da8e840ce22e1312d142285d833fd23
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2021-06-26 20:23:52 -0600

    add error and fetching state to flights

--- src/FlightDetailsBoard.js
+++ src/FlightDetailsBoard.js
@@ -1,35 +1,52 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { FlightDetailsCardPropTypes } from "./FlightDetailsCard";
-import styles from "./FlightDetailsBoard.module.css";
-import FlightDetailsCard from "./FlightDetailsCard";
-import logo from "./imgs/InterimaginaryDepartures-logo.png";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { FlightDetailsCardPropTypes } from './FlightDetailsCard'
+import styles from './FlightDetailsBoard.module.css'
+import FlightDetailsCard from './FlightDetailsCard'
+import logo from './imgs/InterimaginaryDepartures-logo.png'
+import { useSelector } from 'react-redux'
 
 const propTypes = {
-  flights: PropTypes.arrayOf(PropTypes.shape(FlightDetailsCardPropTypes))
-};
+    flights: PropTypes.arrayOf(PropTypes.shape(FlightDetailsCardPropTypes)),
+}
+
+const FetchingFlights = () => (
+    <div className={styles.fetching}>Fetching Flights...</div>
+)
+const FetchingFlightError = () => (
+    <div className={styles.error}>Error Fetching Flights</div>
+)
 
 /** Dumb render only component */
 const FlightDetailsBoard = ({ flights }) => {
-  return (
-    <div className={styles.board}>
-      <div className={styles.title}>
-        <img alt="Interimaginary Departures" src={logo} />
-      </div>
-      <div className={styles.cards}>
-        {flights.map((flight,index) => (
-          <FlightDetailsCard
-            key={flight.destination}
-            {...flight}
-            showUnderline={false}
-            carrier={<img src={flight.carrier} alt="Carrier" />}
-        />
-        ))}
-      </div>
-    </div>
-  );
-};
+    const isFetching = useSelector((state) => state.flights.isFetching)
+    const fetchingError = useSelector((state) => state.flights.error)
+    return (
+        <div className={styles.board}>
+            <div className={styles.title}>
+                <img alt="Interimaginary Departures" src={logo} />
+            </div>
 
-FlightDetailsBoard.propTypes = propTypes;
+            <div className={styles.cards}>
+                {isFetching ? (
+                    <FetchingFlights></FetchingFlights>
+                ) : fetchingError ? (
+                    <FetchingFlightError />
+                ) : (
+                    flights.map((flight, index) => (
+                        <FlightDetailsCard
+                            key={flight.destination}
+                            {...flight}
+                            showUnderline={false}
+                            carrier={<img src={flight.carrier} alt="Carrier" />}
+                        />
+                    ))
+                )}
+            </div>
+        </div>
+    )
+}
 
-export default FlightDetailsBoard;
+FlightDetailsBoard.propTypes = propTypes
+
+export default FlightDetailsBoard
