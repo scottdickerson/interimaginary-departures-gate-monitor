28080c60b2ec36f7c087be96bc7ac53bdb045cbe
t duncan <trayduncan@gmail.com>
2021-10-24 17:38:29 -0500

    Revert "Merge branch 'master' of https://github.com/scottdickerson/interimaginary-departures-gate-monitor"
    This reverts commit b00e5ad703cd93d5ab9f48feecfbdcc0566b9a39, reversing
    changes made to 889f019ca45e2d4170ac545908287736523c7d0a.

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ import isEmpty from 'lodash/isEmpty'
 import { fetchAllFlights } from './api/FlightsActions'
 import { useSelector, useDispatch } from 'react-redux'
 
-const TEST_GATE_DELAY = 50000
-
 /**
  * NOTE: all the flight delay stuff (setFlightDelay) is for the test mode where we run across all the times
  * @returns
@@ -51,7 +49,7 @@ function App() {
                 )
                 setCurrentDay(moment().day())
             },
-            TEST_MODE ? TEST_GATE_DELAY : 10000
+            TEST_MODE ? 30000 : 10000
         )
         return () => clearInterval(interval)
     }, [TEST_MODE, setCurrentTime])
--- src/FlightDetailsBoard.js
+++ src/FlightDetailsBoard.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { FlightDetailsCardPropTypes } from './FlightDetailsCard'
 import styles from './FlightDetailsBoard.module.css'
 import FlightDetailsCard from './FlightDetailsCard'
-//import logo from './imgs/InterimaginaryDepartures-logo.png'
+import logo from './imgs/InterimaginaryDepartures-logo.png'
 import { useSelector } from 'react-redux'
 
 const propTypes = {
@@ -24,8 +24,7 @@ const FlightDetailsBoard = ({ flights }) => {
     return (
         <div className={styles.board}>
             <div className={styles.title}>
-                {/*<img alt="Interimaginary Departures" src={logo} />*/}
-                <h2>Interimaginary Departures</h2>
+                <img alt="Interimaginary Departures" src={logo} />
             </div>
 
             <div className={styles.cards}>
